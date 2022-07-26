0200ad6f1286cca4e2d3055941de65c1c6a03bff
DylanParenzee <dylanparenzee@hotmail.com>
2022-07-27 09:02:24 +1000

    added basic navbar component

---
+++ src/NavBar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <div className="nav-container">
+      <h1 id="logo">Dylan's Image Gallery</h1>
+      <ul className="navigation-items">
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  );
+};
+
+export default NavBar;
