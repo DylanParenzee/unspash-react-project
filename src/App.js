eb8ec90441f385c7459a9c2807763f00af4b75b7
DylanParenzee <dylanparenzee@hotmail.com>
2022-07-28 18:55:21 +1000

    added fetch

--- src/App.js
+++ src/App.js
@@ -3,6 +3,15 @@ import "./index.css";
 import NavBar from "./NavBar";
 
 const App = () => {
+  const accessKey = "Gl9gxcrGbgAsfxKYvsEZAd7DVGWj2C9GLrPc89oJdew";
+  const fetchImages = () => {
+    fetch("https://api.unsplash.com/photos/?client_id=" + accessKey)
+      .then((res) => res.JSON())
+      .then((data) => {
+        console.log(data);
+      });
+  };
+
   return (
     <div className="App">
       <NavBar />
