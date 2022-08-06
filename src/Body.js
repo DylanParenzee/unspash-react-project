03d2942c81899edfc3c02923fef0207f176ad08d
DylanParenzee <dylanparenzee@hotmail.com>
2022-08-07 08:51:21 +1000

    added funactionality for infinite scroll

--- src/Body.js
+++ src/Body.js
@@ -8,36 +8,58 @@ const accessKey = process.env.REACT_APP_ACCESS_KEY;
 const mainUrl = "https://api.unsplash.com/photos";
 const searchUrl = "https://api.unsplash.com/search/photos";
 
+//Main Home Component
 const Home = () => {
-  const [loading, setLoading] = useState(false);
   const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
 
+  //Fetch function for fetching from API
   const fetchImages = () => {
-    setLoading(true);
     let url;
-    url = `${mainUrl}${accessKey}`;
+    const pageURL = `&page=${page}`;
+
+    //setting the URL to fetch from -- doing it like this so I can use The search bar later since its a different URL
+    url = `${mainUrl}${accessKey}${pageURL}`;
     fetch(url)
       .then((res) => {
+        //Throw error if the status of the API call is not 200
         if (res.status !== 200) {
           throw new Error(res.status);
         }
+        //Return the JSON object
         return res.json();
       })
+
+      //Use the data from the returned JSON to set our images state variable
       .then((data) => {
-        console.log(data);
-        setImages(data);
-        setLoading(false);
+        //Had to do this because on scroll event it jsut tries to load the same data again
+        setImages((oldImages) => {
+          //return the old images that were already on the page, with the new ones after the scroll event
+          return [...oldImages, ...data];
+        });
       })
+
+      //this willl be the error logged if status is not 200 on API call
       .catch((error) => {
         console.log(`${error} - unable to load data`);
-        setLoading(false);
       });
   };
 
+  //Had to add this useEffect to run the Fetch function when the page number state variable changes changes
   useEffect(() => {
     fetchImages();
-  }, []);
+  }, [page]);
 
+  useEffect(() => {
+    const event = window.addEventListener("scroll", () => {
+      if (window.innerHeight + window.scrollY >= document.body.scrollHeight) {
+        setPage((oldPage) => {
+          return oldPage + 1;
+        });
+      }
+    });
+  }, []);
+  // This is the click event for the submit button in the search bar
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log("Hello");
@@ -62,7 +84,7 @@ const Home = () => {
       </section>
       <section className="photos">
         <div className="photos-contain">
-          {images.map((image, index) => {
+          {images.map((image) => {
             return <ImageContainer key={image.id} {...image} />;
           })}
         </div>
