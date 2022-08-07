8b4aa0ed95710518e01b02096c91ccaad2bc3cb5
DylanParenzee <dylanparenzee@hotmail.com>
2022-08-08 08:24:22 +1000

    added mailing list

--- src/NavBar.js
+++ src/NavBar.js
@@ -2,9 +2,6 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const NavBar = () => {
-  const handleSubmit = () => {
-    console.log("hello");
-  };
   return (
     <div className="nav-container">
       <h1 className="logo"> - Dylan's API Project -</h1>
