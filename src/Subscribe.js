fddb7e4b140492c48fb7a9a43f2a9a706191083d
DylanParenzee <dylanparenzee@hotmail.com>
2022-08-09 07:24:39 +1000

    added post functionality

--- src/Subscribe.js
+++ src/Subscribe.js
@@ -1,22 +1,41 @@
 import React from "react";
 import Footer from "./Footer";
+import { useState } from "react";
 
 const Subscribe = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("hello");
+    const client = { userEmail };
+
+    fetch("http://localhost:8000/clients", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(client),
+    })
+      .then(() => {
+        console.log("User Email Added");
+        alert("Your email has been added, Thanks :)");
+      })
+      .catch((error) => {
+        console.log(error + "Failed to add Email");
+      });
   };
+
+  const [userEmail, setUserEmail] = useState("");
+
   return (
     <div className="subscribe-page">
       <div className="subscribe-container">
         <h1 className="subscribe-title"> Let's Stay in Touch </h1>
         <h2>Access to 5000+ artist's and their work</h2>
 
-        <form className="subscribe-form" action="">
+        <form className="subscribe-form">
           <input
             className="email-input"
             type="text"
             placeholder="Enter Your Email Address"
+            value={userEmail}
+            onChange={(e) => setUserEmail(e.target.value)}
           />
           <button onClick={handleSubmit} className="subscribe-button">
             Subscribe
